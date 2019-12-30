7e302b0338d0d3b046e8a24bfb4d58e3ff86e2c3
hy787574044 <a787574044@qq.com>
2019-12-30 11:11:05 +0800

    12.30上午更新

--- team03_code/order/src/main/java/com/order/details/pojo/Detail.java
+++ team03_code/order/src/main/java/com/order/details/pojo/Detail.java
@@ -1,12 +1,14 @@
 package com.order.details.pojo;
 
+import com.order.orderings.pojo.Ordering;
+
 /**
- * 订单类
+ * 订单详情类
  */
 public class Detail {
     private Integer did;//订单详情ID
     private Integer fid;//菜品ID
-    private Integer oid;//订单总表ID
+    private Ordering ordering;//引用订单表
     private String dname;//当前菜品名
     private Float dprice;//菜品当前价格
     private String dphoto;//菜品图片
@@ -17,10 +19,10 @@ public class Detail {
 
     }
 
-    public Detail(Integer did, Integer fid, Integer oid, String dname, Float dprice, String dphoto, Integer quantity, String beiyong) {
+    public Detail(Integer did, Integer fid, Ordering ordering, String dname, Float dprice, String dphoto, Integer quantity, String beiyong) {
         this.did = did;
         this.fid = fid;
-        this.oid = oid;
+        this.ordering = ordering;
         this.dname = dname;
         this.dprice = dprice;
         this.dphoto = dphoto;
@@ -44,12 +46,12 @@ public class Detail {
         this.fid = fid;
     }
 
-    public Integer getOid() {
-        return oid;
+    public Ordering getOrdering() {
+        return ordering;
     }
 
-    public void setOid(Integer oid) {
-        this.oid = oid;
+    public void setOrdering(Ordering ordering) {
+        this.ordering = ordering;
     }
 
     public String getDname() {
