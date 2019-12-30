47c1d1975db0c8f094db5fff907c4733353f7927
hy787574044 <a787574044@qq.com>
2019-12-30 09:52:09 +0800

    12.30出版

--- team03_code/order/src/main/java/com/order/userses/controller/UsersesController.java
+++ team03_code/order/src/main/java/com/order/userses/controller/UsersesController.java
@@ -5,19 +5,60 @@ import com.order.userses.service.impl.UsersesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/users")
 public class UsersesController {
     @Autowired
     private UsersesServiceImpl usersesService;
-    public ResponseEntity<Integer> query(User user){
-        User u1=usersesService.query(user);
-        if(u1!=null){
-            return new ResponseEntity<>(1, HttpStatus.OK);
+
+//    public ResponseEntity<Integer> query(User user){
+////        User u1=usersesService.query(user);
+////        if(u1!=null){
+////            return new ResponseEntity<>(1, HttpStatus.OK);
+////        }
+////        return new ResponseEntity<>(0,HttpStatus.UNAUTHORIZED);
+////    }
+//添加用户
+@RequestMapping(value = "/user",method = RequestMethod.POST)
+public ResponseEntity<?> addChef(@PathVariable User user){
+    int i=usersesService.addUser(user);
+    if (i!=1){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+    return new ResponseEntity<>(Integer.valueOf(i), HttpStatus.OK);
+}
+    //删除用户
+    @RequestMapping(value = "/{uid}",method = RequestMethod.DELETE)
+    public ResponseEntity<?> delChef(@PathVariable("uid")Integer uid){
+        int i =usersesService.delUser(uid);
+        if (i!=1){
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
         }
-        return new ResponseEntity<>(0,HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(Integer.valueOf(i),HttpStatus.OK);
+    }
+    //修改用户信息
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public ResponseEntity<?> updateChef(@PathVariable User user){
+        int i=usersesService.updateUser(user);
+        if (i!=1){
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(Integer.valueOf(i),HttpStatus.OK);
+    }
+    //带条件分页查询
+    @RequestMapping(value = {"/{page}/{pageSize}/{keyword}","/{page}/{pageSize}"},method = RequestMethod.GET)
+    public ResponseEntity<List<User>> find(@PathVariable("page") Integer page, @PathVariable("pageSize") Integer pageSize, @PathVariable(name="keyword",required = false)String keyword){
+        List<User> chefList = usersesService.findUser(page,pageSize,keyword);
+        if (chefList.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(chefList, HttpStatus.OK);
     }
 }
--- team03_code/order/src/main/java/com/order/userses/service/impl/UsersesServiceImpl.java
+++ team03_code/order/src/main/java/com/order/userses/service/impl/UsersesServiceImpl.java
@@ -4,20 +4,14 @@ import com.order.userses.mapper.UsersesMapper;
 import com.order.userses.pojo.User;
 import com.order.userses.service.UsersesService;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-
 import java.util.List;
-
-=======
 import org.springframework.stereotype.Service;
 
 @Service
->>>>>>> 7a4dbc5e6ecf0630f5250f962eb1cd2e0987926c
 public class UsersesServiceImpl implements UsersesService {
     @Autowired
     private UsersesMapper usersesMapper;
     @Override
-<<<<<<< HEAD
     public int addUser(User user) {
         return usersesMapper.addUser(user);
     }
@@ -47,10 +41,6 @@ public class UsersesServiceImpl implements UsersesService {
         //计算查询范围
         int start = (page - 1) * pageSize;
         int size = pageSize;
-        return usersesMapper.findUser(start,size,keyword);
-=======
-    public User query(User user) {
-        return usersesMapper.query(user);
->>>>>>> 7a4dbc5e6ecf0630f5250f962eb1cd2e0987926c
+        return usersesMapper.findUser(start, size, keyword);
     }
 }
