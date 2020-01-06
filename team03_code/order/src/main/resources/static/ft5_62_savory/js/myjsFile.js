a5e4e3929553f979490a7be2ce729b6938c5b617
yhgbv213 <58098841+yhgbv213@users.noreply.github.com>
2020-01-06 17:16:59 +0800

    123

--- team03_code/order/src/main/resources/static/ft5_62_savory/js/myjsFile.js
+++ team03_code/order/src/main/resources/static/ft5_62_savory/js/myjsFile.js
@@ -16,8 +16,8 @@ function seleAll() {
             let ds = '';
             $.each(data.data, (i, v) => {
                 ds += `
-                <li>
-					<div class="menu-img"><img src="images/pic.png" width="55" height="55" /></div>
+                <li value="${v.foods.id}">
+					<div class="menu-img"><img src="${v.foods.photo}" width="55" height="55" /></div>
 						<div class="menu-txt">
 							<h4>${v.foods.name}</h4>
 							<p class="list1">${v.foods.info}</p>
@@ -45,10 +45,33 @@ function seleAll() {
 }
 
 
+function seleTp(xx) {
+    $.ajax({
+        method: 'get',
+        url: '',
+        success: function (data) {
+            console.log()
+            let ds = '';
+            $.each(data.data, (i, v) => {
+                ds += `
+                <li class="active">${v.name}<span class="num-price"></span></li>
+                `
+            });
+            $('#a1').html(ds);
+        },
+        dataType: 'json'
+    })
+}
+
+
+
+
+
 let vm = new Vue({
     el: '#app',
     data: {
-
+        sj:[],
+        
     },
 
 })
